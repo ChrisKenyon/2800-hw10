2877ad23a35636245c760d2dd5491ccd78b896af
Izaak Branch <branch.izaak@gmail.com>
2017-03-28 22:45:16 -0400

    Added some missing justifications

--- hw10.lisp
+++ hw10.lisp
@@ -1,61 +1,3 @@
-; **************** BEGIN INITIALIZATION FOR ACL2s B MODE ****************** ;
-; (Nothing to see here!  Your actual file is after this initialization code);
-
-#|
-Pete Manolios
-Fri Jan 27 09:39:00 EST 2012
-----------------------------
-
-Made changes for spring 2012.
-
-
-Pete Manolios
-Thu Jan 27 18:53:33 EST 2011
-----------------------------
-
-The Beginner level is the next level after Bare Bones level.
-
-|#
-
-; Put CCG book first in order, since it seems this results in faster loading of this mode.
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "ccg/ccg" :uncertified-okp nil :dir :acl2s-modes :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
-
-;Common base theory for all modes.
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "base-theory" :dir :acl2s-modes)
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "custom" :dir :acl2s-modes :uncertified-okp nil :ttags :all)
-
-;Settings common to all ACL2s modes
-(acl2s-common-settings)
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading trace-star and evalable-ld-printing books.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "trace-star" :uncertified-okp nil :dir :acl2s-modes :ttags ((:acl2s-interaction)) :load-compiled-file nil)
-(include-book "hacking/evalable-ld-printing" :uncertified-okp nil :dir :system :ttags ((:evalable-ld-printing)) :load-compiled-file nil)
-
-;theory for beginner mode
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s beginner theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "beginner-theory" :dir :acl2s-modes :ttags :all)
-
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s Beginner mode.") (value :invisible))
-;Settings specific to ACL2s Beginner mode.
-(acl2s-beginner-settings)
-
-; why why why why 
-(acl2::xdoc acl2s::defunc) ; almost 3 seconds
-
-(cw "~@0Beginner mode loaded.~%~@1"
-    #+acl2s-startup "${NoMoReSnIp}$~%" #-acl2s-startup ""
-    #+acl2s-startup "${SnIpMeHeRe}$~%" #-acl2s-startup "")
-
-
-(acl2::in-package "ACL2S B")
-
-; ***************** END INITIALIZATION FOR ACL2s B MODE ******************* ;
-;$ACL2s-SMode$;Beginner
 #|
 
 CS 2800 Homework 10 - Spring 2017
@@ -760,8 +702,7 @@ t
 |#
 
 (defthm L1 (implies (and (lorp l)(not (endp l)))
-                    (equal (second (cons a l))(first l))))#|ACL2s-ToDo-Line|#
-
+                    (equal (second (cons a l))(first l))))
 #|
 Lemma 1 proof
 C1. (lorp l)
@@ -913,7 +854,7 @@ C7. (rationalp e)/\(rationalp r)/\(lorp (rest l))/\(in e (filter-less r (rest l)
 -----------------
 C8. (lorp (rest l)) {C1,C4,first-rest ax}
 C9. (in e (filter-less r (rest l))) {def. filter-less,C1,C6}
-C10. (< e r)
+C10. (< e r) {C7, C3, C2, C8, C9, MP}
 (< e r)
 = {C10}
 t
@@ -1008,7 +949,7 @@ C3. (not (endp (rest l)))
 C4. (lorp (filter-less (first l) (rest l))) => (sortedp (qsort (filter-less (first l) (rest l))))
 ---------------
 C5. (lorp (filter-less (first l) (rest l))) {filter-less o.c.}
-C6. (sortedp (qsort (filter-less (first l) (rest l))))
+C6. (sortedp (qsort (filter-less (first l) (rest l)))) {c4, c5, MP}
 (sortedp (qsort l))
 = {C1,C2,C3,def.qsort}
 (sortedp (app (qsort (filter-less (first l)(rest l)))
